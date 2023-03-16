8a8a8499e2518d62044c0278f358744513e28870
Márton Elek <elek@apache.org>
2023-03-16 13:35:01 +0100

    payer subcommand for using payer for balance/transact

---
+++ cmd/crybapy/cmd_payer.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+)
+
+type payerCommandConfig struct {
+	*rootConfig
+	PayerConfig
+}
+
+func newPayerCommand(rootConfig *rootConfig) *cobra.Command {
+	config := &payerCommandConfig{
+		rootConfig:  rootConfig,
+		PayerConfig: PayerConfig{},
+	}
+	cmd := &cobra.Command{
+		Use:   "payer",
+		Short: "Access to the low-level payer commands",
+	}
+	cmd.AddCommand(newPayerBalanceCommand(config))
+	return cmd
+}
